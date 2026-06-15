6fe3f0f9582aaac29777690f029c60cea262127a
DevFIFA <alltaken1982@gmail.com>
2026-06-15 14:02:08 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// You can swap this anytime without touching code
+const DATA_SOURCE = {
+  primary:
+    "https://raw.githubusercontent.com/YOUR_USERNAME/fifa-2026-live/main/data/schedule.json"
+};
